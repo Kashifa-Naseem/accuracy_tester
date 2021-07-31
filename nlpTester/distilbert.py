6cf984524d55ebce4d72b4dfb42bd3aa23ace0b9
Kashifa <kashifanaseem90@gmail.com>
2021-07-31 19:00:17 +0530

    initial commit

--- nlpTester/distilbert.py
+++ nlpTester/distilbert.py
@@ -1,17 +1,16 @@
 import pandas
 
-def predictandsave(model, msg):
-    predic = []
-    text = []
-    for i in msg:
-        text.append(i)
-        prediction = model.predict(i)
-        predic.append(prediction)
-    
+def predictandsave(model, msg, cat):
+    text = [i for i in msg]
+    cat = [i for i in cat]
+    predic = [model.predict(i) for i in msg]
+    result = ['Pass' if cat[i].lower()==predic[i].lower() else 'Fail' for i in range(len(cat))]
     df = pandas.DataFrame()
     df['Text'] = text
+    df['Expected_Category'] = cat
     df['Prediction'] = predic
-
+    df['Result'] = result
+    print('done!')
     return df.to_csv('Predictions.csv')
 
             
